1df6d6fbcddd0dbc9b70591c3d0544b71978e036
David Rolle <david.rolle@gmail.com>
2018-12-02 09:25:13 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.example/no_symlinks "1.0"
+  :description "A Polylith workspace."
+  :plugins [[polylith/lein-polylith "0.2.1"]]
+  :polylith {:top-namespace "com.example"
+             :clojure-version "1.9.0"})
