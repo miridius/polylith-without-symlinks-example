2ecabbd3cee64ec2e2ee3f2f47e2cc5e18688158
David Rolle <david.rolle@gmail.com>
2018-12-02 18:23:27 +0100

    Switched to creating profiles from a config map (but kept the old code there as an alternative)

--- project.clj
+++ project.clj
@@ -38,6 +38,34 @@
     {}
     profiles))
 
+(defn parse-includes [{:keys [includes] :as profile}]
+  (if includes
+    (conj includes (dissoc profile :includes))
+    profile))
+
+(defn interface-profiles [top-namespace]
+  {:interfaces     {:version      "1.0"
+                    :description  "Component interfaces"
+                    :source-paths ["src/interfaces"]
+                    :aot          :all}
+   :interfaces-jar {:dependencies [[(symbol top-namespace "interfaces") "1.0"]]
+                    :aot          :all}})
+
+(defn create-profiles [{:keys [top-namespace] :as config}]
+  (reduce-kv
+    (fn [acc type-kw things]
+      (merge acc
+        (reduce-kv
+          (fn [acc id-kw profile]
+            (let [type (name type-kw)
+                  id (name id-kw)
+                  kw (keyword type id)]
+              (assoc acc kw (update-profile type id (parse-includes profile)))))
+          {}
+          things)))
+    (interface-profiles top-namespace)
+    (dissoc config :top-namespace)))
+
 (defproject com.example/no-symlinks "1.0"
   :description "An example of how a Polylith workspace might look without symlinks."
 
@@ -53,8 +81,38 @@
   ;; This will make each profile use its own target subfolder
   :target-path "target/%s"
 
-  ;; We use profiles instead of individual project.clj files
+  ;; Create profiles from a config map
   :profiles
+  ~(create-profiles
+     '{:top-namespace "com.example"
+
+       :environments
+       {:dev
+        {:description "The main development environment."
+         :includes    [:bases/mybase :components/mycomponent]
+         :global-vars {*warn-on-reflection* true}
+         :plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]]
+         :test-refresh {:changes-only true}}}
+
+       :bases
+       {:mybase
+        {:description  "A mybase base."
+         :dependencies [[com.taoensso/timbre "4.10.0"]]
+         :requires     [:mycomponent]}}
+
+       :components
+       {:mycomponent
+        {:description "A mycomponent component."}}
+
+       :systems
+       {:mysystem
+        {:description "A mysystem system."
+         :version     "0.1"
+         :includes    [:bases/mybase :components/mycomponent]
+         :main        com.example.mybase.core}}})
+
+  ;; Alternatively, augment existing profiles
+  :profiles-old
   ~(add-defaults
      '{
        ;; ============
