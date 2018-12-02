95d08010484aaba01aedfa8246c8953a98570727
David Rolle <david.rolle@gmail.com>
2018-12-02 17:36:59 +0100

    Factored out common profile settings into convenience functions

--- project.clj
+++ project.clj
@@ -1,3 +1,43 @@
+(defn common-defaults [name]
+  {:name        name
+   :target-path (str "target/" name)})
+
+(defn source-defaults [name]
+  {:source-paths   [(str "src/" name)]
+   :resource-paths [(str "resources/" name)]
+   :test-paths     [(str "test/" name)]})
+
+(defn system-defaults [name {:keys [version] :or {version "0.0"}}]
+  (let [uberjar-name (str name "-" version "-standalone.jar")]
+    {:aot           :all
+     :uberjar-name  uberjar-name
+     :clean-targets [(str "target/" name)
+                     (str "target/uberjar/" uberjar-name)]}))
+
+(defn merge-defaults [type name profile]
+  (merge
+    (common-defaults name)
+    (case type
+      ("bases" "components") (source-defaults name)
+      "systems"              (system-defaults name profile)
+      {})
+    profile))
+
+(defn update-profile [type name profile]
+  (cond
+    (vector? profile) (mapv (partial update-profile type name) profile)
+    (map? profile)    (merge-defaults type name profile)
+    :else profile))
+
+(defn add-defaults [profiles]
+  (reduce-kv
+    (fn [acc kw profile]
+      (let [type (namespace kw)
+            name (name kw)]
+        (assoc acc kw (update-profile type name profile))))
+    {}
+    profiles))
+
 (defproject com.example/no-symlinks "1.0"
   :description "An example of how a Polylith workspace might look without symlinks."
 
@@ -15,87 +55,65 @@
 
   ;; We use profiles instead of individual project.clj files
   :profiles
-  {
-   ;; ============
-   ;; Environments
-   ;; ============
+  ~(add-defaults
+     '{
+       ;; ============
+       ;; Environments
+       ;; ============
 
-   ;; We could instead write `:environments/development`, but calling it
-   ;; `:dev` means lein turns it on by default (and so does Cursive)
-   :dev
-   [;; Specifying a vector of profiles causes them to be merged with this one
-    :bases/mybase
-    :components/mycomponent
-    ;; And of course we can still include our own settings as a map literal
-    {:description  "The main development environment."
-     :global-vars  {*warn-on-reflection* true}
-     :plugins      [[com.jakemccrary/lein-test-refresh "0.23.0"]]
-     :test-refresh {:changes-only true}}]
+       :dev
+       [:bases/mybase
+        :components/mycomponent
+        {:description  "The main development environment."
+         :global-vars  {*warn-on-reflection* true}
+         :plugins      [[com.jakemccrary/lein-test-refresh "0.23.0"]]
+         :test-refresh {:changes-only true}}]
 
-   ;; ==========
-   ;; Interfaces
-   ;; ==========
+       ;; ==========
+       ;; Interfaces
+       ;; ==========
 
-   ;; Use this profile to build and install the interface jar:
-   ;; `lein with-profile interfaces install`
-   ;; Or use it directly when compiling a base/component:
-   ;; `lein with-profile interfaces,bases/mybase compile`
-   :interfaces
-   {:name         "interfaces"
-    :version      "1.0"
-    :description  "Component interfaces"
-    :source-paths ["src/interfaces"]
-    :aot          :all}
+       ;; Use this profile to build and install the interface jar:
+       ;; `lein with-profile interfaces install`
+       ;; Or use it directly when compiling a base/component:
+       ;; `lein with-profile interfaces,bases/mybase compile`
+       :interfaces
+       {:version      "1.0"
+        :description  "Component interfaces"
+        :source-paths ["src/interfaces"]
+        :aot          :all}
 
-   ;; Use this profile to use the installed interface jar when compiling:
-   ;; `lein with-profile interfaces-jar,bases/mybase compile`
-   ;; This way you only need to compile the interfaces once.
-   :interfaces-jar
-   {:dependencies [[com.example/interfaces "1.0"]]
-    :aot          :all}
+       ;; Use this profile to use the installed interface jar when compiling:
+       ;; `lein with-profile interfaces-jar,bases/mybase compile`
+       :interfaces-jar
+       {:dependencies [[com.example/interfaces "1.0"]]
+        :aot          :all}
 
-   ;; =====
-   ;; Bases
-   ;; =====
+       ;; =====
+       ;; Bases
+       ;; =====
 
-   :bases/mybase
-   {:description    "A mybase base."
-    ;; deps specified in a base/component are automatically merged into any
-    ;; environment or system that uses it.
-    :dependencies   [[com.taoensso/timbre "4.10.0"]]
-    :source-paths   ["src/mybase"]
-    :resource-paths ["resources/mybase"]
-    :test-paths     ["test/mybase"]}
+       :bases/mybase
+       {:description  "A mybase base."
+        :dependencies [[com.taoensso/timbre "4.10.0"]]}
 
-   ;; ==========
-   ;; Components
-   ;; ==========
+       ;; ==========
+       ;; Components
+       ;; ==========
 
-   :components/mycomponent
-   {:description    "A mycomponent component."
-    :source-paths   ["src/mycomponent"]
-    :resource-paths ["resources/mycomponent"]
-    :test-paths     ["test/mycomponent"]}
+       :components/mycomponent
+       {:description "A mycomponent component."}
 
-   ;; =======
-   ;; Systems
-   ;; =======
+       ;; =======
+       ;; Systems
+       ;; =======
 
-   ;; Run with: `lein with-profile systems/mysystem run`
-   ;; Build with: `lein with-profile systems/mysystem uberjar`
-   :systems/mysystem
-   [;; Specifying a vector of profiles causes them to be merged
-    :bases/mybase
-    :components/mycomponent
-    {:name         "mysystem"
-     :version      "0.1"
-     :description  "A mysystem system."
-     :aot          :all
-     :main         com.example.mybase.core
-     ;; The below settings are necessary for the uberjar task only.
-     ;; If we don't set the uberjar name it will use the project name
-     :uberjar-name "mysystem-0.1-standalone.jar"
-     ;; If we don't specify the clean paths it will clean everything
-     :target-path "target/mysystem"
-     :clean-targets ["target/mysystem"
-                     "target/uberjar/mysystem-0.1-standalone.jar"]}]})
+       ;; Run with: `lein with-profile systems/mysystem run`
+       ;; Build with: `lein with-profile systems/mysystem uberjar`
+       ;; Test with: `lein with-profile systems/mysystem test`
+       :systems/mysystem
+       [:bases/mybase
+        :components/mycomponent
+        {:version     "0.1"
+         :description "A mysystem system."
+         :main        com.example.mybase.core}]}))
